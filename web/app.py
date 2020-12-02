35dd9eb0907337c955b20029cc288964600d7dac
Robert Wallner <robert@scinova.net>
2020-12-03 01:32:44 +0200

    changes

--- web/app.py
+++ web/app.py
@@ -2,11 +2,12 @@ import sys
 sys.path.append('..')
 
 from flask import Flask
-from flask import render_template, send_from_directory
+from flask import render_template, send_from_directory, request, redirect
 
 import common
 import hebrew_numbers
 import re
+import unicodedata
 
 numbers = [hebrew_numbers.int_to_gematria(x) for x in range(0, 151)]
 
@@ -70,13 +71,106 @@ def view_tractate(order_no, tractate_no):
 def zohar():
 	return render_template('zohar.html', zohar=common.Zohar)
 
+@app.route('/zohar-articles')
+def zohar_articles():
+	data = open('../db/zohar/1.01/00.txt').read().split('\n')
+	articles = data
+	return render_template('zohar-articles.html', articles=articles)
+
+@app.route('/zohar/<int:book_ind>/<int:chapter_no>/<int:article_no>')
+def view_zohar_article(book_ind, chapter_no, article_no):
+	book = common.Zohar[book_ind - 1]
+	chapter = book.chapters[chapter_no - 1]
+	t = open('../db/zohar/%1d.%02d/%02d.txt'%(book_ind, chapter_no, article_no)).read()
+	t = re.sub('"([^"]+)"', '“\\1”', t)
+	t = re.sub("'([^']+)'", '‘\\1’', t)
+	ar_texts = t.split('\n\n\n')
+	t = open('../db/zohar/%1d.%02d/%02dt.txt'%(book_ind, chapter_no, article_no)).read()
+	t = re.sub('"([^"]+)"', '“\\1”', t)
+	t = re.sub("'([^']+)'", '‘\\1’', t)
+	he_texts = t.split('\n\n\n')
+	return render_template('zohar-article.html', ar_texts=ar_texts, he_texts=he_texts, re=re,
+		book=book, chapter=chapter, article_no=article_no)
+
+@app.route('/zohar/<int:book_ind>/<int:chapter_no>/<int:article_no>/edit/<int:paragraph_no>', methods=['GET','POST'])
+def edit_zohar_paragraph(book_ind, chapter_no, article_no, paragraph_no):
+	book = common.Zohar[book_ind - 1]
+	chapter = book.chapters[chapter_no - 1]
+	ar_texts = open('../db/zohar/%1d.%02d/%02d.txt'%(book_ind, chapter_no, article_no)).read().split('\n\n\n')
+	he_texts = open('../db/zohar/%1d.%02d/%02dt.txt'%(book_ind, chapter_no, article_no)).read().split('\n\n\n')
+	if request.method == 'POST':
+		ar_texts[paragraph_no - 1] = request.form['ar_text'].replace('\r\n', '\n')
+		he_texts[paragraph_no - 1] = request.form['he_text'].replace('\r\n', '\n')
+		ar_text = '\n\n\n'.join(ar_texts)
+		he_text = '\n\n\n'.join(he_texts)
+		f = '../db/zohar/%1d.%02d/%02d.txt'%(book_ind, chapter_no, article_no)
+		open(f, 'w').write(ar_text)
+		f = '../db/zohar/%1d.%02d/%02dt.txt'%(book_ind, chapter_no, article_no)
+		open(f, 'w').write(he_text)
+		return redirect('/zohar/%d/%d/%d#v%d'%(book_ind, chapter_no, article_no, paragraph_no))
+	if request.method == 'GET':
+		ar_text = ar_texts[paragraph_no - 1]
+		he_text = he_texts[paragraph_no - 1]
+		return render_template('zohar-edit-paragraph.html', ar_text=ar_text, he_text=he_text, re=re,
+				book=book, chapter=chapter, article_no=article_no, paragraph_no=paragraph_no)
+
 @app.route('/zohar/<int:book_ind>/<int:chapter_no>')
 def view_zohar_chapter(book_ind, chapter_no):
 	book = common.Zohar[book_ind - 1]
 	data = open('../db/zohar/%1d.%02d.txt'%(book_ind, chapter_no)).read()
-	chapter = book.chapters[chapter_no - 1]#common.Chapter(1)
+	chapter = book.chapters[chapter_no - 1]
 	verses = data.split('\n')
 	for x in range(len(verses)):
 		verse = common.Verse(x + 1, verses[x])
 		chapter.verses.append(verse)
 	return render_template('zohar-chapter.html', chapter=chapter, book=book)
+
+def remove_cantillation(text):
+	return re.sub('[\u0591-\u05af\u05bd]', '', text)
+
+def remove_punctuation(text):
+	return re.sub('[\u05b0-\u05bc\u05c1\u05c2\u05c7]', '', text)
+
+@app.route('/devel/')
+def devel():
+	data = ''
+	for b in range(len(common.Zohar)):
+		book = common.Zohar[b]
+		for c in range(len(book.chapters)):
+			chapter = book.chapters[c]
+			src = '../db/zohar/%1d.%02d.txt'%(b + 1, c + 1)
+			data += open(src).read()
+			print (src)
+	data = re.sub('\n', ' ', data)
+	data = re.sub('[\(\),\.\:]', ' ', data)
+	data = remove_cantillation(data)
+	words = data.split(' ')
+	words = list(set(words))
+
+	words = [w.replace("''", '״') if w.startswith("''") else w for w in words]
+	names = [hebrew_numbers.int_to_gematria(x, gershayim=False) for x in range(1, 800)]
+	#print ('------', names)
+	names = [''.join(sorted(list(n), reverse=True)) for n in names]
+	#print ('------', names)
+
+	numbers = []
+	for p in ['', 'ב', 'ו', 'וב', 'ד', 'ל', 'מ']:
+		for n in names:
+			numbers.append(p + n)
+#	print (numbers)
+	words = [w.replace("''", '״') if remove_punctuation(w).replace("''", '') in numbers else w for w in words]
+
+	names = ['אלף', 'בית', 'גימל', 'דלת', 'הא', 'ויו', 'ואו', 'זין', 'חית', 'טית', 'יוד', 'כף',
+			 'למד', 'מם', 'נון', 'סמך', 'פא', 'עין', 'צדי', 'קוף','ריש', 'שין', 'תיו']
+	letters = []
+	for p in ['', 'ב', 'ו', 'וב', 'ד', 'ל', 'מ']:
+		for n in names:
+			letters.append(p + n)
+#	print (letters)
+	words = [w.replace("''", "״") if remove_punctuation(w).replace("''", "") in letters else w for w in words]
+
+	words = [w.replace("''", '') for w in words]
+	words = [w for w in words if "'" in w]
+#	print (numbers)
+	words.sort(key=remove_punctuation)
+	return render_template('devel.html', words = words)
