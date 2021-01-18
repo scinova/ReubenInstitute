876e2577268683c7fb639253cb0df7a02e3d0762
Robert Wallner <robert@scinova.net>
2021-01-18 22:10:53 +0200

    rashi fix

--- sources/makedb-rashi.py
+++ sources/makedb-rashi.py
@@ -3,16 +3,22 @@ sys.path.append('..')
 import common
 import re
 
-for i in range(len(common.Bible)):
-	book = common.Bible[i]
-	src = 'rashi/%02d.txt'%(book.ind)
+for book in common.tanakh.books:
+	src = 'rashi/%02d.txt'%(book.number)
 	data = open(src, newline='').read()
-	chapters = re.split('\nChapter [0-9]+\n\n', data)[1:]
-	for c in range(len(chapters)):
-		chapter = chapters[c]
-		verses = re.split('\n\nVerse [0-9]+\n\n', chapter)[1:]
-		out = '\n'.join([verse.replace('\n', ' ') for verse in verses])
-		dst = '../db/rashi/%02d.%03d.txt'%(i + 1, c + 1)
+	chapters_string = re.split('\n\nChapter [0-9]+\n\n', data)[1:]
+	for idx, chapter in enumerate(book.chapters):
+		chapter_string = chapters_string[idx]
+		verses = re.split('\n\nVerse [0-9]+\n\n', chapter_string)[1:]
+		# MISTAKES
+		if book.number == 13 and chapter.number == 31:
+			verses = verses[1:]
+		if book.number == 2 and chapter.number == 38:
+			verses = verses[:31]
+		# MISTAKES END
+		diff = len(chapter.verses) - len(verses)
+		if diff:
+			verses.extend(diff * [''])
+		out = '\n'.join([v.replace('\n', ' ') for v in verses])
+		dst = '../db/rashi/%02d.%03d.txt'%(book.number, chapter.number)
 		open(dst, 'w').write(out)
-		print (c + 1, len(verses), dst)
-
