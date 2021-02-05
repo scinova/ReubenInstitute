db41045c75744a4816b8624f65b0c0606c48277c
Robert Wallner <robert@scinova.net>
2021-02-05 05:57:03 +0200

    remove diacritics from explicit name

--- db/fix-mikra.py
+++ db/fix-mikra.py
@@ -7,10 +7,25 @@ if len(sys.argv) != 3:
 	print ('usage: %s <book_no> <chapter_no>'%sys.argv[0])
 	exit(-1)
 
+def remove_diacritics(text):
+	return re.sub('[\u05b0-\u05b8]', '', text)
+
 book_idx = int(sys.argv[1]) - 1
 chapter_idx = int(sys.argv[2]) - 1
 chapter = common.tanakh.books[book_idx].chapters[chapter_idx]
 
 for verse in chapter.verses:
-	verse.text = re.sub(' ׀', '׀', verse.text)
-	verse.save_mikra()
+	# remove space before siluk
+	text = re.sub(' ׀', '׀', verse.text)
+	# remove diacritics from explicit name
+	name = 'יהוה'
+	pattern = ''
+	for l in range(len(name)):
+		pattern += name[l] + '[\u0591-\u05bd]*'
+	for m in reversed(list(re.finditer(pattern, text))):
+		replacement = remove_diacritics(m.group())
+		text = text[:m.start()] + replacement + text[m.end():]
+
+	if text != verse.text:
+		verse.text = text
+		verse.save_mikra()
