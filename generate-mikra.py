bc1f615ea9f313d2c8733c47edb213123a6b2072
Robert Wallner <robert@scinova.net>
2021-06-14 10:14:26 +0300

    good changes

--- generate-mikra.py
+++ generate-mikra.py
@@ -32,6 +32,7 @@ createCharStyle("Default Character Style", font, 15, fillcolor='Black', language
 createCharStyle("chapterno", font, 10, fillcolor='Silver', language='he')
 createCharStyle("verseno", font, 8, fillcolor='Silver', language='he')
 createCharStyle("mikra", font, 15, fillcolor='Black', language='he')
+createCharStyle("punctuation", font, 15, fillcolor='Blue', language='he')
 createCharStyle("transparent", font, 15, fillcolor='White', language='he')
 createCharStyle("aliya", font2, 10, fillcolor='Silver', language='he')
 createCharStyle("keri", font, 15, fillcolor='Silver', language='he')
@@ -39,17 +40,19 @@ createCharStyle("majuscule", font, 20, fillcolor='Black', language='he')
 createCharStyle("minuscule", font, 12, fillcolor='Black', language='he')
 createCharStyle("cantillation", font, 15, fillcolor='Blue', language='he')
 createCharStyle("dcantillation", font, 15, fillcolor='Red', language='he')
+createCharStyle("punctuation", font, 15, fillcolor='Blue', language='he')
 createCharStyle("translation", font, 12, fillcolor='Black', language='he')
 createCharStyle("ttranslation", font, 9, fillcolor='Black', language='he')
 createCharStyle("nonliteral", font, 12, fillcolor='Red', language='he')
 createCharStyle("addition", font, 11, fillcolor='Blue', language='he')
 
-createParagraphStyle("Default Paragraph Style", alignment=ALIGN_BLOCK, linespacing=20)
-createParagraphStyle("paragraph-opened", alignment=ALIGN_BLOCK, linespacing=20)
-createParagraphStyle("paragraph-closed", alignment=ALIGN_BLOCK, linespacing=20, firstindent=40)
+createParagraphStyle("Default Paragraph Style", linespacingmode=1, alignment=ALIGN_BLOCK)
+createParagraphStyle("mikra", linespacingmode=0, linespacing=18, alignment=ALIGN_BLOCK)
+createParagraphStyle("paragraph-opened", linespacingmode=1, alignment=ALIGN_BLOCK)
+createParagraphStyle("paragraph-closed", linespacingmode=1, alignment=ALIGN_BLOCK, firstindent=40)
 
 def add_text(frame, text, char_style=None, paragraph_style=None):
-	text = common.remove_cantillations(text)
+	#text = common.remove_cantillations(text)
 	length = len(text)
 	pos = getTextLength(frame)
 	insertText(text, pos, frame)
@@ -68,21 +71,23 @@ scribus.progressTotal(len(parasha.verses))
 scribus.progressReset()
 progress = 0
 paragraphs = parasha.paragraphs
-for paragraph in paragraphs[:3]:
+for paragraph in paragraphs[:6]:
 	for subparagraph in paragraph:
+		# MIKRA
 		is_first_subparagraph = subparagraph == paragraph[0]
 		if is_first_subparagraph:
 			paragraph_style = 'paragraph-opened'
 		else:
 			paragraph_style = 'paragraph-closed'
 		for verse in subparagraph:
-			# MIKRA
 			spans = verse.mikra
-			i = 1 * (spans[0].kind == SpanKind.ALIYA)
-			spans.insert(i, Span(SpanKind.VERSENO, verse.hebrew_number))
+			#i = 1 * (spans[0].kind == SpanKind.ALIYA)
+			spans.insert(0, Span(SpanKind.VERSENO, verse.hebrew_number))
 			if verse.number == 1:
-				spans.insert(i, Span(SpanKind.CHAPTERNO, verse.chapter.hebrew_number))
+				spans.insert(0, Span(SpanKind.CHAPTERNO, verse.chapter.hebrew_number))
 			for span in spans:
+				# eplace he + mapik with wide he + mapik
+				span.value = re.sub('\u05d4\u05bc', '\ufb23\u05bc', span.value)
 				if span.kind == SpanKind.CHAPTERNO:
 					add_text(frame, span.value, 'chapterno', paragraph_style)
 					add_text(frame, ' ', 'transparent', paragraph_style)
@@ -104,8 +109,15 @@ for paragraph in paragraphs[:3]:
 			is_last_verse = verse == subparagraph[-1]
 			if not is_last_verse:
 				add_text(frame, ' ')
+		add_text(frame, '\n')
 
-			# TRANSLATIONS
+		# ONKELOS
+		is_first_subparagraph = subparagraph == paragraph[0]
+		if is_first_subparagraph:
+			paragraph_style = 'paragraph-opened'
+		else:
+			paragraph_style = 'paragraph-closed'
+		for verse in subparagraph:
 			spans = verse.onkelos
 			spans.insert(0, Span(SpanKind.VERSENO, verse.hebrew_number))
 			if verse.number == 1:
@@ -123,7 +135,18 @@ for paragraph in paragraphs[:3]:
 					add_text(frame, span.value, 'addition', paragraph_style)
 				elif span.kind == SpanKind.PLAIN:
 					add_text(frame, span.value, 'translation', paragraph_style)
+			is_last_verse = verse == subparagraph[-1]
+			if not is_last_verse:
+				add_text(frame, ' ')
+		add_text(frame, '\n')
 
+		# ONKELOS TRANSLATION
+		is_first_subparagraph = subparagraph == paragraph[0]
+		if is_first_subparagraph:
+			paragraph_style = 'paragraph-opened'
+		else:
+			paragraph_style = 'paragraph-closed'
+		for verse in subparagraph:
 			spans = verse.onkelos_trans
 			spans.insert(0, Span(SpanKind.VERSENO, verse.hebrew_number))
 			if verse.number == 1:
@@ -141,8 +164,18 @@ for paragraph in paragraphs[:3]:
 					add_text(frame, span.value, 'addition', paragraph_style)
 				elif span.kind == SpanKind.PLAIN:
 					add_text(frame, span.value, 'ttranslation', paragraph_style)
+			is_last_verse = verse == subparagraph[-1]
+			if not is_last_verse:
+				add_text(frame, ' ')
+		add_text(frame, '\n')
 
-			# JERUSALMI
+		# JERUSALMI
+		is_first_subparagraph = subparagraph == paragraph[0]
+		if is_first_subparagraph:
+			paragraph_style = 'paragraph-opened'
+		else:
+			paragraph_style = 'paragraph-closed'
+		for verse in subparagraph:
 			spans = verse.jerusalmi
 			spans.insert(0, Span(SpanKind.VERSENO, verse.hebrew_number))
 			if verse.number == 1:
@@ -160,7 +193,18 @@ for paragraph in paragraphs[:3]:
 					add_text(frame, span.value, 'addition', paragraph_style)
 				elif span.kind == SpanKind.PLAIN:
 					add_text(frame, span.value, 'ttranslation', paragraph_style)
+			is_last_verse = verse == subparagraph[-1]
+			if not is_last_verse:
+				add_text(frame, ' ')
+		add_text(frame, '\n')
 
+		# JERUSALMI TRANSLATION
+		is_first_subparagraph = subparagraph == paragraph[0]
+		if is_first_subparagraph:
+			paragraph_style = 'paragraph-opened'
+		else:
+			paragraph_style = 'paragraph-closed'
+		for verse in subparagraph:
 			spans = verse.jerusalmi_trans
 			spans.insert(0, Span(SpanKind.VERSENO, verse.hebrew_number))
 			if verse.number == 1:
@@ -178,23 +222,35 @@ for paragraph in paragraphs[:3]:
 					add_text(frame, span.value, 'addition', paragraph_style)
 				elif span.kind == SpanKind.PLAIN:
 					add_text(frame, span.value, 'translation', paragraph_style)
-
-			progress += 1
-			scribus.progressSet(progress)
+			is_last_verse = verse == subparagraph[-1]
+			if not is_last_verse:
+				add_text(frame, ' ')
 		add_text(frame, '\n')
+
+		progress += 1
+		#scribus.progressSet(progress)
+		#add_text(frame, '\n')
 	add_text(frame, '\n')
 
 setTextDirection(DIRECTION_RTL, frame)
 
 selectText(0, getTextLength(frame), frame)
 content = getAllText(frame)
-cantillations = list(reversed(list(re.finditer(u'[\u0591-\u05af\u05bd\u05c3]+', content))))
+cantillations = list(reversed(list(re.finditer(u'[\u0591-\u05af\u05bd\u05c3\u05c0]+', content))))
+punctuations = list(reversed(list(re.finditer(u'[\.\'\!\?\;\:]+', content))))
 scribus.progressTotal(len(cantillations))
 scribus.progressReset()
 for progress, i in enumerate(cantillations):
 	selectText(i.start(), i.end() - i.start(), frame)
 	setCharacterStyle('dcantillation', frame)
 	scribus.progressSet(progress)
+#scribus.progressTotal(len(punctuations))
+#scribus.progressReset()
+#for progress, i in enumerate(punctuations):
+#	selectText(i.start(), i.end() - i.start(), frame)
+#	setCharacterStyle('punctuation', frame)
+#	scribus.progressSet(progress)
+
 """for i in reversed(tuple(re.finditer(u'[\u0591\u05c3\u0592\u0593\u0594\u0595\u05c0]+', content))):
 	selectText(i.start(), i.end() - i.start(), frame)
 	setCharacterStyle('dcantillation', frame)"""
